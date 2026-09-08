aacfa949201447f46020495ee6a7fe4eb08f4db0
vic0li <149320938+vic0li@users.noreply.github.com>
2026-09-07 22:49:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1478,4 +1478,4 @@ calibração aplicáveis ao modelo.
 
 "causas":["Movimento", "Manguito inadequado", "Vazamento", "Sensor", "Deflação irregular", "Interferência no sinal", "Processamento"],
 
-"passos":["Confirmar condições de uso previstas pelo fabricante.", "Verificar integridade"
+"passos":["Confirmar condições de uso previstas pelo fabricante.", "Verificar integridade"]
